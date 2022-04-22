b9bb79c75c40c2d8d52b09820099e3b4a4b1a79d
Paul Daniel <dantchatchouang@gmail.com>
2022-04-22 17:57:07 +0200

    finish first version of backend setup

--- app.mjs
+++ app.mjs
@@ -5,11 +5,44 @@ import path from 'path'
 import { fileURLToPath } from 'url'
 import PrismicDom from 'prismic-dom'
 import express from 'express'
+
+// INIT
 dotenv.config()
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const app = express()
 const port = 3000
 
+// HELPER FUNCTIONS
+const handleLinkResolver = doc => {
+  if (doc.type === 'product') {
+    return `/detail/${doc.uid}`
+  }
+
+  if (doc.type === 'about') {
+    return '/about'
+  }
+
+  if (doc.type === 'collections') {
+    return '/collections'
+  }
+}
+
+const handleRequest = async () => {
+  try {
+    const meta = await client.getSingle('metadata')
+    const preloader = await client.getSingle('preloader')
+    const navigation = await client.getSingle('navigation')
+    return {
+      meta,
+      navigation,
+      preloader
+    }
+  } catch (error) {
+    console.error('handle Request failed', error)
+  }
+}
+
+// MIDDLEWARE
 // Add a middleware function that runs on every route. It will inject
 // the prismic context to the locals so that we can access these in
 // our templates.
@@ -17,6 +50,7 @@ app.use((req, res, next) => {
   res.locals.ctx = {
     prismicH
   }
+  res.locals.Link = handleLinkResolver
   res.locals.PrismicDom = PrismicDom
   next()
   res.locals.Numbers = index => {
@@ -24,74 +58,73 @@ app.use((req, res, next) => {
   }
 })
 
+// EXPRESS SET CONFIGURATION
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
+// EXPRESS ROUTES
 app.get('/', async (_req, res) => {
   // Here we are retrieving the first document from your API endpoint
   try {
     const home = await client.getSingle('home')
-    const meta = await client.getSingle('metadata')
-    const preloader = await client.getSingle('preloader')
+    const collections = await client.getAllByType('collection')
+    const defaults = await handleRequest()
     res.render('pages/home', {
+      ...defaults,
       home,
-      meta,
-      preloader
+      collections
     })
   } catch (error) {
-    console.error(error)
+    console.error('Get request for homepage failed', error)
   }
 })
+
 app.get('/about', async (_req, res) => {
   try {
     const about = await client.getSingle('about')
-    const meta = await client.getSingle('metadata')
-    const preloader = await client.getSingle('preloader')
+    const defaults = await handleRequest()
     res.render('pages/about', {
-      meta,
-      about,
-      preloader
+      ...defaults,
+      about
     })
   } catch (error) {
-    console.error(error)
+    console.error('Get request for about failed', error)
   }
 })
+
 app.get('/collections', async (_req, res) => {
   try {
-    const meta = await client.getSingle('metadata')
     const collections = await client.getAllByType('collection', {
       fetchLinks: 'product.image'
     })
     const home = await client.getSingle('home')
-    const preloader = await client.getSingle('preloader')
+    const defaults = await handleRequest()
     res.render('pages/collection', {
-      meta,
+      ...defaults,
       collections,
-      home,
-      preloader
+      home
     })
   } catch (error) {
-    console.error(error)
+    console.error('Get request for collections failed', error)
   }
 })
+
 app.get('/detail/:uid', async (req, res) => {
   try {
-    // To get the UID
-  // req.params.uid
     const detail = await client.getByUID('product', req.params.uid, {
       fetchLinks: 'collection.title'
     })
-    const meta = await client.getSingle('metadata')
-    const preloader = await client.getSingle('preloader')
+    const defaults = await handleRequest()
     res.render('pages/detail', {
-      meta,
-      detail,
-      preloader
+      ...defaults,
+      detail
     })
   } catch (error) {
-    console.error(error)
+    console.error('Get request for path details failed', error)
   }
 })
+
+// LAUNCH SERVER
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
