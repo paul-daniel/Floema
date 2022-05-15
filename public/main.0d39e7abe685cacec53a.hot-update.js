4feded4fdec4ffecf1df69db72d0ba471849b73b
Paul Daniel <dantchatchouang@gmail.com>
2022-05-15 08:28:36 +0200

    update navigation

---
+++ public/main.0d39e7abe685cacec53a.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdateFloema"]("main",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("473e6b838e110b8cf93b")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi4wZDM5ZTdhYmU2ODVjYWNlYzUzYS5ob3QtdXBkYXRlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O1VBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9GbG9lbWEvd2VicGFjay9ydW50aW1lL2dldEZ1bGxIYXNoIl0sInNvdXJjZXNDb250ZW50IjpbIl9fd2VicGFja19yZXF1aXJlX18uaCA9ICgpID0+IChcIjQ3M2U2YjgzOGUxMTBiOGNmOTNiXCIpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
